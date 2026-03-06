d1a9fb2b3c093518317f38bcdae5dec635261bf0
GavireddyTejaswini <tejaswinigavireddy@gmail.com>
2026-03-06 10:48:04 +0530

    data.py

--- python/class/data.py
+++ python/class/data.py
@@ -1,6 +1,9 @@
-name=input("enter the name: ")
-city=input("enter the city name: ")
-institute=input("enter the institute name: ")
-course=input("enter the course name: ")
-print(f" i am {name}, came to {city} and joined in {institute} for {course} course")
+# name=input("enter the name: ")
+# city=input("enter the city name: ")
+# institute=input("enter the institute name: ")
+# course=input("enter the course name: ")
+# print(f" i am {name}, came to {city} and joined in {institute} for {course} course")
 
+x=10
+y=20
+print(x)
